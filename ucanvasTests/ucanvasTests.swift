e247fe0f40e08a3f201b31b5628bb5343fdd0e1d
Jl115 <joelbern006@gmail.com>
2025-01-12 16:57:36 +0100

    Initial Commit

---
+++ ucanvasTests/ucanvasTests.swift
@@ -0,0 +1,17 @@
+//
+//  ucanvasTests.swift
+//  ucanvasTests
+//
+//  Created by jldev on 12.01.2025.
+//
+
+import Testing
+@testable import ucanvas
+
+struct ucanvasTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
